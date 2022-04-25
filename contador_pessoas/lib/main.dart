35f2830bcde2c0c00fdfeea7799b2587276f15bf
Luis Santos <luissantos@Mac-mini-de-Luis.local>
2022-04-25 08:48:28 -0300

    Ajustes no app contador-pessoas

--- contador_pessoas/lib/main.dart
+++ contador_pessoas/lib/main.dart
@@ -9,12 +9,102 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: Container(
-        color: Colors.black,
-        alignment: Alignment.center,
-        child: const Text('Hola q tal?!'),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
     );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  void homeTeamGoal() {
+    // ignore: avoid_print
+    print('increment home team');
+  }
+
+  void awayTeamGoal() {
+    // ignore: avoid_print
+    print('increment away team');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: const AssetImage('assets/images/field.jpeg'),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(Colors.white.withOpacity(0.5), BlendMode.modulate,)
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Placar do jogo',
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.w900,
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(height: 70),
+          const Text(
+            '0 x 0',
+            style: TextStyle(
+              fontSize: 70,
+              color: Colors.black,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+          const SizedBox(height: 50),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: homeTeamGoal,
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    primary: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6),
+                    )),
+                child: const Text(
+                  'Gol time \n casa',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 20),
+              TextButton(
+                onPressed: awayTeamGoal,
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    primary: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6),
+                    )),
+                child: const Text(
+                  'Gol time \n fora',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    ));
   }
 }
